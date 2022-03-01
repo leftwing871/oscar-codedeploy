d9795359ff03513ab2db10700d7e9966ca3b852b
Oscar Lee <leftwing871@gmail.com>
2022-03-02 07:00:38 +0900

    Add files via upload

---
+++ hookAfterInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPOSITORY=/var/lib/tomcat/webapps/
+
+date >> /home/ec2-user/log-after.txt
+
+rm -rf $REPOSITORY/*
+
+cp /home/ec2-user/ROOT.war $REPOSITORY
+
+sudo service tomcat restart
---
+++ hookBeforeInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+REPOSITORY=/var/lib/tomcat/webapps
+
+date >> /home/ec2-user/log-before.txt
